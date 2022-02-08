92d99ca8aa09797410dc4de358ef1dadb229f8f9
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-08 23:59:51 +0600

    then scaner class

--- src/First/stdin_stdout.java
+++ src/First/stdin_stdout.java
@@ -1,7 +1,11 @@
 package First;
 
+import java.util.Scanner;
+
 public class stdin_stdout {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
 
     }
 }
