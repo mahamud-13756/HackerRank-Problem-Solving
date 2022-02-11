69e2c2752649928fcb25e2487e2b1dc6c7c08561
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-12 00:10:12 +0600

    This is if else program main method

---
+++ src/First/ifElse.java
@@ -0,0 +1,7 @@
+package First;
+
+public class ifElse {
+    public static void main(String[] args) {
+
+    }
+}
--- src/First/stdin_stdout.java
+++ src/First/stdin_stdout.java
@@ -11,7 +11,6 @@ public class stdin_stdout {
         String d = scan.nextLine();
 
         System.out.println(a);
-
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
