8d9ab3e984d5d989f36d4453c2d97e58d91789a8
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-14 22:03:12 +0600

    Problem pic

--- src/Second/Problem2.java
+++ src/Second/Problem2.java
@@ -1,4 +1,14 @@
 package Second;
 
+import java.util.Scanner;
+
 public class Problem2 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        if(N >= 2 && N<=20){
+
+
+        }
+    }
 }
