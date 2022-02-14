4f971efc44ef473aaaf5ac64e18d49ebdc49a1aa
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-14 20:56:05 +0600

    this is output format

---
+++ src/Second/Problem1.java
@@ -0,0 +1,19 @@
+package Second;
+
+import java.util.Scanner;
+
+public class Problem1 {
+    // https://www.hackerrank.com/challenges/java-output-formatting/problem?isFullScreen=true
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+
+
+
+
+
+        System.out.println("================================");
+
+    }
+}
