23b4a09f7c49086dd9da0c60646ed644e418a011
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-16 22:32:57 +0600

    This is

--- src/Second/Equation_problem.java
+++ src/Second/Equation_problem.java
@@ -1,5 +1,13 @@
 package Second;
+
+import java.util.Scanner;
+
 //https://www.hackerrank.com/challenges/java-loops/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 public class Equation_problem {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+
+    }
 
 }
