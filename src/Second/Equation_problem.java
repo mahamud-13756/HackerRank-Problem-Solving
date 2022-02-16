6ac73a07d23c11df5a3de7885d7d39409a97f258
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-16 22:34:01 +0600

    This is solution

--- src/Second/Equation_problem.java
+++ src/Second/Equation_problem.java
@@ -7,6 +7,19 @@ public class Equation_problem {
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int calc = a;
+            for(int j=0;j<n;j++)
+            {
+                calc+=(Math.pow(2,j)*b);
+                System.out.print(calc+" ");
+            }
+            System.out.println();
+        }
+        in.close();
 
     }
 
