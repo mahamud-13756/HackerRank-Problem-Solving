379c0047b97bb374fed8f35d225992ddc2c92141
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-16 21:35:43 +0600

    This is is right way

--- src/Second/Problem2.java
+++ src/Second/Problem2.java
@@ -3,18 +3,27 @@ package Second;
 import java.util.Scanner;
 
 public class Problem2 {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int N=s.nextInt();
-        if(N >= 2 && N<=20){
-            for(int i=1;i<=10;i++){
-//                int result=N*i;
-                System.out.println(N +" 4x "+i +" = "+ N*i);
-            }
+//    public static void main(String[] args) {
+//        Scanner s=new Scanner(System.in);
+//        int N=s.nextInt();
+//        if(N >= 2 && N<=20){
+//            for(int i=1;i<=10;i++){
+////                int result=N*i;
+//                System.out.println(N +" 4x "+i +" = "+ N*i);
+//
+//
+//            }
+//
+//
+//        }
 
-
-        }
-    }
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int N = in.nextInt();
+            for(int i = 1; i <=10; i++)
+            {
+                System.out.println(N + " x "+i+" = "+i*N);
+    }}
 
 
 //    public static void main(String[] args) {
