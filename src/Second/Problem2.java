8cbb9a36527d23869df8f440d817da2508c95e13
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-16 21:06:39 +0600

    This is new way with Buffering

--- src/Second/Problem2.java
+++ src/Second/Problem2.java
@@ -3,16 +3,21 @@ package Second;
 import java.util.Scanner;
 
 public class Problem2 {
+//    public static void main(String[] args) {
+//        Scanner s=new Scanner(System.in);
+//        int N=s.nextInt();
+//        if(N >= 2 && N<=20){
+//            for(int i=1;i<=10;i++){
+//                int result=N*i;
+//                System.out.println(N +"* "+i +" = "+result);
+//            }
+//
+//
+//        }
+//    }
+
+
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int N=s.nextInt();
-        if(N >= 2 && N<=20){
-            for(int i=1;i<=10;i++){
-                int result=N*i;
-                System.out.println(N +"* "+i +" = "+result);
-            }
 
-
-        }
     }
 }
